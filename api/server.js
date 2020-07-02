b22fab128f15898b1f92674a3e2fef0251162658
unknown <sara.reidy.21@gmail.com>
2020-07-02 11:47:07 -0400

    login and registration working

--- api/server.js
+++ api/server.js
@@ -3,10 +3,13 @@ const helmet = require('helmet')
 const cors = require('cors')
 const server = express()
 
+const authRouter = require('../auth/router.js')
+
 server.use(cors())
 server.use(helmet())
 server.use(express.json())
 
+server.use('/auth', authRouter)
 
 server.get('/', (req, res) => {
     res.status(200).json({api: 'API server is up and running'})
